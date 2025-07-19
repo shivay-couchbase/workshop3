1fabe168d2abc12e5767b85249768c935a481c78
Shivay @ Couchbase <162308263+shivay-couchbase@users.noreply.github.com>
2025-07-19 12:12:31 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,14 @@
 # Start of the script
 x = 5
 y = 2
-
+a = 4
 # Intentional IndentationError
     z = x + y
 
 # This line has a runtime error (undefined variable)
 print(result)
+
+
+
+# Intentional IndentationError
+    b = a + y
